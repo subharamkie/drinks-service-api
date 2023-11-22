2e4627f27eb415d67652ac82148d929a91be9c39
Subha <subharamkie@gmail.com>
2023-11-22 14:24:41 +0000

    Add:Added 2 test cases for /coffee to check different input and empty query obj

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -8,3 +8,23 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee should return correct object", async () => {
+  const res = await request(app)
+    .get("/coffee")
+    .query({ coffeeName: "Americano" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Americano",
+  });
+});
+
+test("GET /coffee should return correct object", async () => {
+  const res = await request(app).get("/coffee").query({});
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
